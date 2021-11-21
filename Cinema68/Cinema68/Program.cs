d65e5f9fb5ea373242ba55e3dce2f878d2e8d9d8
AJ Chiong <anselmojosechiong2000@gmail.com>
2021-11-21 15:45:59 -0500

    Successfully connected login boundary with login control.

--- Cinema68/Cinema68/Program.cs
+++ Cinema68/Cinema68/Program.cs
@@ -16,7 +16,7 @@ namespace Cinma68
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new StartupPage());
         }
     }
 }
